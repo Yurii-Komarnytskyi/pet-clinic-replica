d5c4d3239d947930ca91dfa465f72531e4ca2e3e
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-02-23 05:52:24 -0500

    Basic CRUD interface

---
+++ pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package com.ykomarnytskyi2022.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+	 
+	Set<T> findAll();
+	
+	T findById(ID id);
+	
+	T save(T object);
+	
+	void delete(T object);
+	
+	void deleteByID(ID id);
+}
