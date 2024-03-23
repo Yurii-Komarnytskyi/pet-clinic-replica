1fcd69312fa42b1202ccd1f7604879789a29a893
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-03-23 09:29:16 -0400

    Created Vet Speciality entity; Associated to Vet Closes #25

---
+++ pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package com.ykomarnytskyi2022.petclinic.model;
+
+public class Speciality extends BaseEntity {
+	
+	private String specialityDescription;
+
+	public String getSpecialityDescription() {
+		return specialityDescription;
+	}
+
+	public void setSpecialityDescription(String specialityDescription) {
+		this.specialityDescription = specialityDescription;
+	}
+
+	
+}
--- pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/Vet.java
@@ -1,5 +1,18 @@
 package com.ykomarnytskyi2022.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+	private Set<Speciality> specialities;
+
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
+	
+	
 }
