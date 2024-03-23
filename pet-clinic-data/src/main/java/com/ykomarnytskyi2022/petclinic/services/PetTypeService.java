84fee833d29d136093dad59ea6bd312ef79643d1
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-03-23 10:58:15 -0400

    Created PetType Map Service Implementation closed #27

---
+++ pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.ykomarnytskyi2022.petclinic.services;
+
+import com.ykomarnytskyi2022.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
