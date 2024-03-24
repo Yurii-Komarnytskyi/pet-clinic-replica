2652aa82b11076c9d5148c677ef24230c2054ef2
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-03-24 10:13:09 -0400

    Added name to Pet, made petType Optional

--- pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/Pet.java
@@ -1,32 +1,49 @@
 package com.ykomarnytskyi2022.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
-public class Pet extends BaseEntity{
-	
-	private PetType petType;
+public class Pet extends BaseEntity {
+
+	private Optional<PetType> petType = Optional.empty();
 	private Owner owner;
+	private String name;
 	private LocalDate birthDay;
-	
-	public PetType getPetType() {
+
+	public Optional<PetType> getPetType() {
 		return petType;
 	}
+
 	public void setPetType(PetType petType) {
-		this.petType = petType;
+		try {
+			this.petType = Optional.of(petType);
+		} catch (NullPointerException e) {
+			throw new RuntimeException("petType CANNOT be null");
+		}
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public Owner getOwner() {
 		return owner;
 	}
+
 	public void setOwner(Owner owner) {
 		this.owner = owner;
 	}
+
 	public LocalDate getBirthDay() {
 		return birthDay;
 	}
+
 	public void setBirthDay(LocalDate birthDay) {
 		this.birthDay = birthDay;
 	}
-	
-	
-	
+
 }
