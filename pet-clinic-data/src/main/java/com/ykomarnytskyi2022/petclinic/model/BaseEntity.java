94d028aa6ab926b5c6274ad0a79fdab31db6c0b8
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-04-06 09:39:25 -0400

    Implemented Base entity (JPA); closes #33

--- pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ykomarnytskyi2022/petclinic/model/BaseEntity.java
@@ -2,8 +2,16 @@ package com.ykomarnytskyi2022.petclinic.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
