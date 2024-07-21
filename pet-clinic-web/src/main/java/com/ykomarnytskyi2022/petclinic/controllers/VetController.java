ccceb87acf295b109d86cb81b31d5ba8190519cb
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-07-21 11:25:02 +0200

    implemented Vets JSON endpoint closes #61

--- pet-clinic-web/src/main/java/com/ykomarnytskyi2022/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ykomarnytskyi2022/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.ykomarnytskyi2022.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ykomarnytskyi2022.petclinic.model.Vet;
 import com.ykomarnytskyi2022.petclinic.services.VetService;
 
 @Controller
@@ -15,11 +20,14 @@ public class VetController {
 		this.vetService = vetService;
 	}
 
-
-
 	@RequestMapping({"/vets" , "/vets/index", "/vets/index.html", "/vets.html"})
 	public String listVets(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJSON() {
+		return vetService.findAll();
 	}
 }
